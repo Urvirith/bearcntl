3eec131613597c07045f74a6f9f3a8e237276083
Jake Mussler <jake.mussler@protonmail.com>
2022-04-06 17:24:28 +0100

    Timer moves with system clock.

--- main/src/main.c
+++ main/src/main.c
@@ -13,13 +13,13 @@ extern void SystemInit() {
     rcc_write_ahb2_enr(RCC, RCC_GPIOB_AHB2EN);          // ENABLE GPIOB
     rcc_write_ahb2_enr(RCC, RCC_GPIOC_AHB2EN);          // ENABLE GPIOC
     rcc_set_pll(RCC, Rcc_MSI, 48, 0);                   // SET MULTIPLIER AND DIVIDER FOR PLL
-    rcc_set_pllclk(RCC, Rcc_PLL8);                      // SET DIVIDER FOR PLLCLK
+    rcc_set_pllclk(RCC, Rcc_PLL6);                      // SET DIVIDER FOR PLLCLK
     rcc_start_pll(RCC);                                 // START PLL
     rcc_set_pllclk_en(RCC);                             // SET PLLCLK OUTPUT
     rcc_write_ccipr1(RCC, RCC_FDCAN_CCIPR1_PLLQCLK);    // SELECT CCIPR1 FDCAN PLLQCLK
     rcc_write_apb1_enr2(RCC, RCC_FDCAN_APB1_ENR2);      // ENABLE RCC FDCAN CLOCK
     rcc_write_apb1_enr1(RCC, RCC_TIMER2_APB1R1EN);      // ENABLE TIMER 2
-    //rcc_sys_clk(RCC, 3);
+    rcc_sys_clk(RCC, 3);
 }
 
 extern void main() {
@@ -27,7 +27,7 @@ extern void main() {
     gpio_type(GPIOB, LED_BLU_PIN, Gpio_Output, Gpio_Push_Pull, AF0);
     gpio_type(GPIOC, LED_GRN_PIN, Gpio_Output, Gpio_Push_Pull, AF0);
     timer_open(TIMER2, Timer_Cont, Timer_Upcount);
-    timer_set_time(TIMER2, 1000, 4000, 500);
+    timer_set_time(TIMER2, 1000, 32000, 10000);
     timer_start(TIMER2);
 
     u32 i = 0;
